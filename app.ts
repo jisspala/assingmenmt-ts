a27d2633e3ece8b9fdc661e1ffcc0a7b3a6f95ff
Jiss Jose <32099292+jisspala@users.noreply.github.com>
2023-05-13 19:15:42 +0200

    adding app file and mocking functionality
    adding app.ts , mocking the functionality and expecting the required out put

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log('creating order processor');
+import { processOrders } from './service';
+
+const orderFile = 'input/order.csv';
+
+async function app() {
+  const ProcessedResult = await processOrders(orderFile);
+
+  if (ProcessedResult.success) {
+    for (const item of ProcessedResult.data) {
+      console.log(item.join(', '));
+    }
+  } else {
+    console.log(ProcessedResult.message);
+  }
+}
+
+app();
--- service.ts
+++ service.ts
@@ -1,15 +1,20 @@
 interface ProcessedResult {
   success: boolean;
-  data?: [];
+  data?: string[][];
   message?: string;
 }
 
 export const processOrders = async (
   orderFile: string
 ): Promise<ProcessedResult> => {
+  const finalResults = [
+    ['heart: 0', 'liver: 2', 'lung: 2'],
+    ['heart: 0', 'liver: 2', 'lung: 2'],
+  ];
+
   const processedResult: ProcessedResult = {
     success: true,
-    data: [],
+    data: finalResults,
   };
   return processedResult;
 };
