bcb2824442cc339e324ab775429a8050de140fc9
Jiss Jose <32099292+jisspala@users.noreply.github.com>
2023-05-14 23:17:07 +0200

    test: adding more tests
     adding more tests to cover more situations

--- tests/service.test.ts
+++ tests/service.test.ts
@@ -1,5 +1,7 @@
 import { processOrders } from '../service';
 import { ProcessedResult } from '../utils/interfaces';
+import constants from '../utils/stringConstants';
+
 let filePath = 'tests/test_assets/correct_orders.csv';
 
 afterEach(() => {
@@ -9,26 +11,58 @@ afterEach(() => {
 describe('processOrders', () => {
   test('should return correct result status and data', async () => {
     filePath = 'tests/test_assets/correct_orders.csv';
+    const expectedResult = [
+      ['heart: 0', 'liver: 2', 'lung: 2'],
+      ['heart: 4', 'liver: 0', 'lung: 0'],
+      ['heart: 0', 'liver: 0', 'lung: 1'],
+    ];
     const processedOrders: ProcessedResult = await processOrders(filePath);
     expect(processedOrders.success).toBe(true);
     expect(processedOrders.data?.length).toBeGreaterThan(0);
+    expect(processedOrders.data).toEqual(expectedResult);
   });
+
+  test('should return correct result status and data for the organ which dont have any bonus ', async () => {
+    filePath = 'tests/test_assets/correct_orders_new_organ.csv';
+    const expectedResult = [
+      ['heart: 0', 'liver: 2', 'lung: 2'],
+      ['heart: 0', 'liver: 0', 'lung: 0', 'neworgan: 3'],
+    ];
+
+    const processedOrders: ProcessedResult = await processOrders(filePath);
+    expect(processedOrders.success).toBe(true);
+    expect(processedOrders.data?.length).toBeGreaterThan(0);
+    expect(processedOrders.data).toEqual(expectedResult);
+  });
+
   test('should return error message and status when order file is not exists', async () => {
     filePath = 'nofile';
     const processedOrders: ProcessedResult = await processOrders(filePath);
     expect(processedOrders.success).toBe(false);
-    expect(processedOrders.message).toBe('error in orderFile file reading');
+    expect(processedOrders.message).toBe(constants.INVALID_CSV);
   });
   test('should return error message and status when order bonus_ratio is zero', async () => {
     filePath = 'tests/test_assets/orders_zero_bonus_ratio.csv';
     const processedOrders: ProcessedResult = await processOrders(filePath);
     expect(processedOrders.success).toBe(false);
-    expect(processedOrders.message).toBe('orders are not valid');
+    expect(processedOrders.message).toBe(constants.INVALID_ORDERS);
   });
   test('should return error message and status when order price is invalid', async () => {
     filePath = 'tests/test_assets/orders_invalid_price.csv';
     const processedOrders: ProcessedResult = await processOrders(filePath);
     expect(processedOrders.success).toBe(false);
-    expect(processedOrders.message).toBe('orders are not valid');
+    expect(processedOrders.message).toBe(constants.INVALID_ORDERS);
+  });
+  test('should return error message and status when order price is invalid', async () => {
+    filePath = 'tests/test_assets/orders_invalid_price.csv';
+    const processedOrders: ProcessedResult = await processOrders(filePath);
+    expect(processedOrders.success).toBe(false);
+    expect(processedOrders.message).toBe(constants.INVALID_ORDERS);
+  });
+  test('should return error message and status when order file is empty', async () => {
+    filePath = 'tests/test_assets/no_data.csv';
+    const processedOrders: ProcessedResult = await processOrders(filePath);
+    expect(processedOrders.success).toBe(false);
+    expect(processedOrders.message).toBe(constants.INVALID_ORDERS);
   });
 });
